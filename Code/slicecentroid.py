302bc1c82d35d7b6f8de5c66b36da10dafb8ebd5
David Fuentes <fuentesdt@gmail.com>
2016-02-23 21:39:16 -0600

    BUG: exception handling

--- Code/slicecentroid.py
+++ Code/slicecentroid.py
@@ -13,38 +13,36 @@ parser.add_option( "--imagefile",
 if (options.imagefile != None):
   getHeaderCmd = 'c3d %s -centroid ' % options.imagefile
   maskimage   = options.imagefile
-  imageprefix = '/'.join(options.imagefile.split('/')[:-1])
-  timeid = '.'.join(options.imagefile.split('/')[-1].split('.')[1:])
-  if ( len( imageprefix)):
-    truthimage = "%s/Truth.%s" % (imageprefix, timeid )
-  else:
-    truthimage = "./Truth.%s" % ( timeid )
+  truthimage  = maskimage.replace('Mask','Truth')
   #print  truthimage 
-  if ( os.path.isfile(truthimage) ): # check training set available
-   try: # necrosis first
-     getHeaderCmd = 'c3d %s -thresh 3 3 1 0  -centroid ' % truthimage 
-     #print getHeaderCmd
-     headerProcess = subprocess.Popen(getHeaderCmd ,shell=True,stdout=subprocess.PIPE )
-     while ( headerProcess.poll() == None ):
-        pass
-     rawcentroidinfo = headerProcess.stdout.readline().strip('\n')
-     centroid = eval( rawcentroidinfo.replace('CENTROID_VOX',''))
-   except NameError: # enhancing second
-     getHeaderCmd = 'c3d %s -thresh 2 2 1 0  -centroid ' % truthimage 
-     #print getHeaderCmd
-     headerProcess = subprocess.Popen(getHeaderCmd ,shell=True,stdout=subprocess.PIPE )
-     while ( headerProcess.poll() == None ):
-        pass
-     rawcentroidinfo = headerProcess.stdout.readline().strip('\n')
-     centroid = eval( rawcentroidinfo.replace('CENTROID_VOX',''))
-  else: #use mask
-    getHeaderCmd = 'c3d %s -thresh 1 1 1 0  -centroid ' % maskimage 
+  # necrosis first
+  try: 
+    getHeaderCmd = 'c3d %s -thresh 3 3 1 0  -centroid ' % truthimage 
     #print getHeaderCmd
     headerProcess = subprocess.Popen(getHeaderCmd ,shell=True,stdout=subprocess.PIPE )
     while ( headerProcess.poll() == None ):
        pass
     rawcentroidinfo = headerProcess.stdout.readline().strip('\n')
     centroid = eval( rawcentroidinfo.replace('CENTROID_VOX',''))
+  except NameError: 
+    # enhancing second
+    try: 
+      getHeaderCmd = 'c3d %s -thresh 2 2 1 0  -centroid ' % truthimage 
+      #print getHeaderCmd
+      headerProcess = subprocess.Popen(getHeaderCmd ,shell=True,stdout=subprocess.PIPE )
+      while ( headerProcess.poll() == None ):
+         pass
+      rawcentroidinfo = headerProcess.stdout.readline().strip('\n')
+      centroid = eval( rawcentroidinfo.replace('CENTROID_VOX',''))
+    except NameError: 
+      #use mask
+      getHeaderCmd = 'c3d %s -thresh 1 1 1 0  -centroid ' % maskimage 
+      #print getHeaderCmd
+      headerProcess = subprocess.Popen(getHeaderCmd ,shell=True,stdout=subprocess.PIPE )
+      while ( headerProcess.poll() == None ):
+         pass
+      rawcentroidinfo = headerProcess.stdout.readline().strip('\n')
+      centroid = eval( rawcentroidinfo.replace('CENTROID_VOX',''))
   #print getHeaderCmd
   #print centroid , int(centroid [2])
   print int(centroid [2])
